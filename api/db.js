f09e5d21120a13f63ee33a1dd2f5ff78123f6d0f
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2024-08-23 12:56:12 +0200

    remove api folder

--- api/db.js
+++
@@ -1,9 +0,0 @@
-import mysql from "mysql2";
-
-export const db = mysql.createConnection({
-  host: "192.168.1.102", // Only the IP address or hostname here
-  port: 3306, // Specify the port number separately
-  user: "matteof",
-  password: "1234567890",
-  database: "blog",
-});
