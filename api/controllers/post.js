faf2fc727d8b51792d21b6172bd72b4c49a72df6
matteofazzeri <112771288+matteofazzeri@users.noreply.github.com>
2024-08-23 23:27:25 +0200

    tentativo

--- api/controllers/post.js
+++ api/controllers/post.js
@@ -92,7 +92,7 @@ export const updatePost = async (req, res) => {
   try {
     const token = req.cookies.access_token;
 
-    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    if (!token) return res.status(401).json({ message: "Unauthorized" + token });
 
     const user = jwt.verify(token, process.env.JWT_SECRET);
 
